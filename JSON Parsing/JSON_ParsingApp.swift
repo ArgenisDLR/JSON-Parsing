6426dfce1428f2d5e01f73b1e09df8e44ffa660a
Argenis De La Rosa <oftherose91@gmail.com>
2021-02-11 07:29:48 -0500

    Initial Commit

---
+++ JSON Parsing/JSON_ParsingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSON_ParsingApp.swift
+//  JSON Parsing
+//
+//  Created by Argenis De La Rosa on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct JSON_ParsingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
